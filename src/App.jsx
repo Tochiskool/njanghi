74c79dbe05092a5b9c41cca0513e4451da0e8019
Tochiskool <mrchristian79@yahoo.com>
2024-04-25 15:06:08 +0200

    add:deploy workflow

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+const App = () => {
+  
+  return ( 
+    <>
+         <header className="background-primary">Header Content</header>
+              <main>
+                   <section className="background-secondary">Hero Primary Content</section>
+                        <section>Image and Text Content</section>
+                              <section>Featured Content</section>
+                              <section className="background-primary">Testimonial Content</section>
+                         <section className="background-secondary">Media Objects</section>
+                   <section className="background-tertiary">More Content</section>
+              </main>
+              <footer className="background-primary">Footer Content</footer>
+    </>
+  )
+}
+
+export default App
