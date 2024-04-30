ea3e447d2905b4b0220e643c39e257f9c89e5c8a
Tochiskool <mrchristian79@yahoo.com>
2024-04-30 14:06:10 +0200

    Logo updated

--- src/routes/Hero/Hero.jsx
+++ src/routes/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React,{ useState } from "react"
-import image from "./Images/acn.jpeg"
+import image from "./Images/acn2.jpeg"
 import imageHero from "./Images/hero.jpeg"
 import Navbar from '../Navbar/Navbar'
 import "./Css/hero.css"
@@ -22,7 +22,7 @@ const Hero = () => {
              <header className="header-primary">
                 <div className="flexing">
                 <a className="logo logo-primary">
-                <img src={image} alt="Working desk"/>
+                <img className="round" src={image} alt="Working desk"/>
                 </a>        
                 <div className='btn btn-primary btn-toggle-menu'>
                  <FontAwesomeIcon icon={faBars} />
@@ -34,9 +34,9 @@ const Hero = () => {
                 <img src={imageHero} />
                   <div className="container">
                       <div className="hero-body">
-                          <h1 className="hero-title">Hero Primary Title Content</h1>
-                          <h4 className="hero-subtitle">Hero Primary Subtitle Content</h4>
-                          <button className="btn">Button Text</button>
+                          <h1 className="hero-title">Welcome to my Official Portfolio</h1>
+                          <h4 className="hero-subtitle">Full stack web developer</h4>
+                          <button className="btn">Text me</button>
                       </div>
                   </div>
             </section>
